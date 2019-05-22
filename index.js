a6fa52b23fc6a02b4111f14e5e6808f87ff0fe54
Samantha brake <sammy.brake@gmail.com>
2019-05-22 23:29:33 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -2,6 +2,8 @@ function produceDrivingRange(range) {
   return function(numberOne, numberTwo) {
     let distanceOne = parseInt(numberOne);
     let distanceTwo = parseInt(numberTwo);
+    let testRange = Math.abs(distanceTwo - distanceOne);
+    
   };
 };
 
